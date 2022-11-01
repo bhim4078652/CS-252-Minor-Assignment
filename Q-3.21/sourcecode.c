4ee230822986d8e74ff960c7b84e2e2d79311675
bhimreddy <91386363+bhim4078652@users.noreply.github.com>
2022-11-01 12:45:28 +0530

    Update sourcecode.c

--- Q-3.21/sourcecode.c
+++ Q-3.21/sourcecode.c
@@ -1,34 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h> 
+#include <stdio.h>  // library for standard input output operations.
+#include <sys/types.h> // library which contains implementation of fork system call.
+#include <unistd.h> // library which helps in defining datatype pid_t.
+#include <sys/wait.h> //library for wait system call.
 
 int main()
 {
-	int n=0;
-	int k=0;
-	
-	int status;
-	pid_t pid;
+	int k=0; 
+	pid_t pid; 
 
+// variable pid of datatype pid_t which is used to store returned value from from fork system call.
+
+// reading non negative integer from user
 	do
 	{
 		printf("Please enter a number greater than 0 to run the Collatz Conjecture.\n"); 
   		scanf("%d", &k);	
 	}while (k <= 0);
 	
+/*fork system call is used to create child process, here we are creating child processes to calculate and 
+  print the  collatz conjecture*/
 
 	pid = fork();
 	
+	// if os fails to create child process then -1 is returned by fork system call
+
 	if(pid==-1)
 	{
 		printf("failed to create child process\n");
 	}
 	
-	
+	/* child process will be assigned pid variable value to zero and parent process will get pid value which 
+	   is nothing but process id of child process. */
+
 	else if (pid == 0)
 	{
+
+		// below is the algorithm for implemetation of Collatz Conjecture problem.
 		printf("Child is working...\n");
 		printf("%d\n",k);
 		while (k!=1)
@@ -54,6 +61,9 @@ int main()
 	
 	else
 	{
+		/* this section will be only entered by parent process hence will call wait system call
+		   so that child process can finish it job first and then parent continues. */
+		   
 		printf("Parents is waiting on child process...\n");
 		wait(NULL);
 		printf("Parent process is done.\n");
@@ -61,4 +71,3 @@ int main()
 	
 	return 0; 
 }
-
